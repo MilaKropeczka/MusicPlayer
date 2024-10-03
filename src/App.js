9a1fac11ff2592a774f34c4fa46ae9b4fc6eb7be
MilaKropeczka <milakropeczka@gmail.com>
2024-10-03 14:45:08 +0200

    feat: add favorites icon to SongList component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import Searchbar from './components/Searchbar/Searchbar';
 import SongList from './components/SongList/SongList';
-import { useEffect } from 'react';
 import { ResultsProvider } from './context/ResultsContext';
 import useSpotifyToken from './hooks/useSpotifyToken';
 
 function App() {
 	const { accessToken, error } = useSpotifyToken();
-
-	useEffect(() => {
-		if (accessToken) {
-			console.log('Spotify Access Token:', accessToken);
-		}
-	}, [accessToken]);
 
 	if (error) return <div>Error: {error}</div>;
 	if (!accessToken) return <div>Loading...</div>;
--- src/components/SongList/SongList.jsx
+++ src/components/SongList/SongList.jsx
@@ -1,50 +1,26 @@
 import React, { useContext, useEffect, useState } from 'react';
 import styles from './SongList.module.css';
-import img from '../../assets/1.jpg';
-import img2 from '../../assets/2.jpg';
 import ResultsContext from '../../context/ResultsContext';
 
 export default function SongList() {
 	const { results } = useContext(ResultsContext);
-	const [songs, setSongs] = useState([
-		{
-			id: 1,
-			img: img,
-			title: 'My Life - Imagine Dragons ',
-			views: '1543 obserwujących',
-			time: '02:23',
-		},
-		{
-			id: 2,
-			img: img2,
-			title: 'Bones - Imagine Dragons',
-			views: '16543 obserwujących',
-			time: '03:10',
-		},
-		{
-			id: 3,
-			img: img,
-			title: 'My Life - Imagine Dragons ',
-			views: '1543 obserwujących',
-			time: '02:23',
-		},
-		{
-			id: 4,
-			img: img2,
-			title: 'Bones - Imagine Dragons',
-			views: '16543 obserwujących',
-			time: '03:10',
-		},
-	]);
+	const [songs, setSongs] = useState([]);
+	const [favourite, setFavourite] = useState(true);
 
 	useEffect(() => {
-		results.map((el) => {
-			// console.log(el);
-			console.log(el.album.images[0].url);
-			// console.log(el.artists[0].name);
-			// console.log(el.name);
-		});
-	});
+		if (results) {
+			const newSongs = results.map((el, index) => {
+				return {
+					id: index + 1,
+					img: el.album.images[0].url,
+					title: `${el.name} - ${el.artists[0].name}`,
+					time: el.duration_ms,
+				};
+			});
+			setSongs(newSongs);
+		}
+	}, [results]);
+
 	return (
 		<div className={styles.container}>
 			<h2 className={styles.heading}>Lista ulubionych utworów</h2>
@@ -53,17 +29,20 @@ export default function SongList() {
 					<li key={song.id} className={styles.item}>
 						<span className={styles.number}>{index + 1}</span>
 						<img
-							src={
-								'https://i.scdn.co/image/ab67616d0000b2734efc70ac7d68e4ba8172cc4d'
-							}
-							// src={song.img}
-							// src={img5}
+							src={song.img}
 							alt={song.title}
 							className={styles.img}
 						/>
 						<span className={styles.title}>{song.title}</span>
 						<span className={styles.views}>{song.views}</span>
 						<span className={styles.time}>{song.time}</span>
+						<span className={styles.favourite}>
+							{favourite ? (
+								<i className='fa-solid fa-heart'></i>
+							) : (
+								<i className='fa-regular fa-heart'></i>
+							)}
+						</span>
 					</li>
 				))}
 			</ul>
